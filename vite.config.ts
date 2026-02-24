bae73465e7a9c78036a4beb880e9dacac8eeb9c9
lukassstrader-hash <lukasss.trader@gmail.com>
2026-02-25 06:23:26 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
-    target: "es2015",
-    minify: "esbuild",
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600,
-    rollupOptions: {
-      external: [],
-    },
+    outDir: "dist",
   },
 });
